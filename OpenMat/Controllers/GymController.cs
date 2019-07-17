8fb7d83a148273a1a782ee22ebc7bb810302336a
Wayne Collier <ke4tri@gmail.com>
2019-07-17 13:16:40 -0500

    getting gyms listed from api via insomnia

---
+++ OpenMat/Controllers/GymController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenMat.Data;
+
+namespace OpenMat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GymController : ControllerBase
+    {
+        readonly GymRepository _gymRepository;
+
+        public GymController()
+        {
+            _gymRepository = new GymRepository();
+        }
+
+        [HttpGet]
+
+        public ActionResult GetAllProducts()
+        {
+            var user = _gymRepository.GetAll();
+
+            return Ok(user);
+        }
+
+    }
+}
