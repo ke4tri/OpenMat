17c6f1f04de1b07197399723a2c3b6fffbe3aef4
Wayne Collier <ke4tri@gmail.com>
2019-08-01 19:49:42 -0500

    style on the letters on landing page

--- OpenMat/ClientApp/src/components/Home/Home.js
+++ OpenMat/ClientApp/src/components/Home/Home.js
@@ -2,16 +2,34 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import "./Home.css";
 
+
 export class Home extends React.Component {
+
+  componentDidMount(){
+    let spans = document.querySelectorAll('.word span');
+    spans.forEach((span, idx) => {
+    
+      span.addEventListener('click', (e) => {
+        e.target.classList.add('active');
+      });
+      
+      span.addEventListener('animationend', (e) => {
+        e.target.classList.remove('active');
+        
+      });
+      
+      
+      setTimeout(() => {
+        span.classList.add('active');
+      }, 750 * (idx+1))
+    });
+  }
 
   render() {
     return (
       <div className="divWrap homeImage">
       <div className="classLink ">
         <div className="homeLinks ">
-          {/* <div>
-            <Link to="/" className="test"></Link>
-          </div> */}
           <div>
             <Link to="/about" className="test">ABOUT</Link>
           </div>
@@ -20,8 +38,17 @@ export class Home extends React.Component {
           </div>         
         </div>
 
-        <div className="homeH1">
-          <div className="openMatText">OPEN MAT</div>
+        <div className="homeH1 aniHome">
+          <div className="word">
+              <span>O</span>
+              <span>P</span>
+              <span>E</span>
+              <span>N</span>
+              <span>-</span>
+              <span>M</span>
+              <span>A</span>
+              <span>T</span>
+          </div>
         </div> 
        </div>
        </div>
